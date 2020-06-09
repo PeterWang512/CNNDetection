d7d00748967fe7f54161caab879fce6ca5ebd8cc
Richard Zhang <rizhang@rizhangs-MacBook-Pro.local>
2020-06-09 14:28:25 -0700

    multiple subdirs

--- demo_dir.py
+++ demo_dir.py
@@ -11,24 +11,30 @@ from sklearn.metrics import average_precision_score, precision_recall_curve, acc
 
 from networks.resnet import resnet50
 
+from IPython import embed
+
+from tqdm import tqdm
+
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-d','--dir', type=str, default='examples_realfakedir')
+parser.add_argument('-d','--dir', nargs='+', type=str, default='examples_realfakedir')
 parser.add_argument('-m','--model_path', type=str, default='weights/blur_jpg_prob0.5.pth')
 parser.add_argument('-b','--batch_size', type=int, default=32)
 parser.add_argument('-j','--workers', type=int, default=4, help='number of workers')
 parser.add_argument('--crop', type=int, default=None, help='by default, do not crop. specify crop size')
 parser.add_argument('--use_cpu', action='store_true', help='uses gpu by default, turn on to use cpu')
+parser.add_argument('--size_only', action='store_true', help='only look at sizes of images in dataset')
 
 opt = parser.parse_args()
 
 # Load model
-model = resnet50(num_classes=1)
-if(opt.model_path is not None):
-    state_dict = torch.load(opt.model_path, map_location='cpu')
-model.load_state_dict(state_dict['model'])
-model.eval()
-if(not opt.use_cpu):
-    model.cuda()
+if(not opt.size_only):
+  model = resnet50(num_classes=1)
+  if(opt.model_path is not None):
+      state_dict = torch.load(opt.model_path, map_location='cpu')
+  model.load_state_dict(state_dict['model'])
+  model.eval()
+  if(not opt.use_cpu):
+      model.cuda()
 
 # Transform
 trans_init = []
@@ -40,30 +46,45 @@ trans = transforms.Compose(trans_init + [
 ])
 
 # Dataset loader
-dataset = datasets.ImageFolder(opt.dir, transform=trans)
-data_loader = torch.utils.data.DataLoader(dataset,
+if(type(opt.dir)==str):
+  opt.dir = [opt.dir,]
+
+print('Loading [%i] datasets'%len(opt.dir))
+data_loaders = []
+for dir in opt.dir:
+  dataset = datasets.ImageFolder(opt.dir[0], transform=trans)
+  data_loaders+=[torch.utils.data.DataLoader(dataset,
                                           batch_size=opt.batch_size,
                                           shuffle=False,
-                                          num_workers=opt.workers)
+                                          num_workers=opt.workers),]
 
 y_true, y_pred = [], []
+Hs, Ws = [], []
 with torch.no_grad():
-	for data, label in data_loader:
-	    if(not opt.use_cpu):
-	        data = data.cuda()
-	    y_pred.extend(model(data).sigmoid().flatten().tolist())
-	    y_true.extend(label.flatten().tolist())
+  for data_loader in data_loaders:
+    for data, label in tqdm(data_loader):
+    # for data, label in data_loader:
+      Hs.append(data.shape[2])
+      Ws.append(data.shape[3])
 
+      y_true.extend(label.flatten().tolist())
+      if(not opt.size_only):
+        if(not opt.use_cpu):
+            data = data.cuda()
+        y_pred.extend(model(data).sigmoid().flatten().tolist())
+
+Hs, Ws = np.array(Hs), np.array(Ws)
 y_true, y_pred = np.array(y_true), np.array(y_pred)
-r_acc = accuracy_score(y_true[y_true==0], y_pred[y_true==0] > 0.5)
-f_acc = accuracy_score(y_true[y_true==1], y_pred[y_true==1] > 0.5)
-acc = accuracy_score(y_true, y_pred > 0.5)
-ap = average_precision_score(y_true, y_pred)
 
-print(y_true)
-print(y_pred)
+print('Average sizes: [{:2.2f}+/-{:2.2f}] x [{:2.2f}+/-{:2.2f}] = [{:2.2f}+/-{:2.2f} Mpix]'.format(np.mean(Hs), np.std(Hs), np.mean(Ws), np.std(Ws), np.mean(Hs*Ws)/1e6, np.std(Hs*Ws)/1e6))
+print('Num reals: {}, Num fakes: {}'.format(np.sum(1-y_true), np.sum(y_true)))
 
-print('# reals: {}, # fakes: {}'.format(np.sum(1-y_true), np.sum(y_true)))
-print('AP: {:2.2%}, Acc: {:2.2%}, Acc (real): {:2.2%}, Acc (fake): {:2.2%}'.format(ap, acc, r_acc, f_acc))
+if(not opt.size_only):
+  r_acc = accuracy_score(y_true[y_true==0], y_pred[y_true==0] > 0.5)
+  f_acc = accuracy_score(y_true[y_true==1], y_pred[y_true==1] > 0.5)
+  acc = accuracy_score(y_true, y_pred > 0.5)
+  ap = average_precision_score(y_true, y_pred)
+
+  print('AP: {:2.2%%}, Acc: {:2.2%%}, Acc (real): {:2.2%%}, Acc (fake): {:2.2%%}'.format(ap, acc, r_acc, f_acc))
 
 
