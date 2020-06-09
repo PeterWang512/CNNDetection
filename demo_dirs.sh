8842d0aeebc246b608235b9160943d038f758d03
Richard Zhang <rizhang@rizhangs-MacBook-Pro.local>
2020-06-09 14:46:22 -0700

    script

---
+++ demo_dirs.sh
@@ -0,0 +1,52 @@
+
+echo ''
+echo biggan
+~/anaconda3/bin/python demo_dir.py -d examples2/biggan -b 1
+
+echo ''
+echo crn
+~/anaconda3/bin/python demo_dir.py -d examples2/crn -b 1
+
+echo ''
+echo cyclegan
+~/anaconda3/bin/python demo_dir.py -d examples2/cyclegan/* -b 1
+
+echo ''
+echo deepfake
+~/anaconda3/bin/python demo_dir.py -d examples2/deepfake -b 1
+
+echo ''
+echo gaugan
+~/anaconda3/bin/python demo_dir.py -d examples2/gaugan -b 1
+
+echo ''
+echo imle
+~/anaconda3/bin/python demo_dir.py -d examples2/imle -b 1
+
+echo ''
+echo progan
+~/anaconda3/bin/python demo_dir.py -d examples2/progan/* -b 1
+
+echo ''
+echo san
+~/anaconda3/bin/python demo_dir.py -d examples2/san -b 1
+
+echo ''
+echo seeingdark
+~/anaconda3/bin/python demo_dir.py -d examples2/seeingdark -b 1
+
+echo ''
+echo stargan
+~/anaconda3/bin/python demo_dir.py -d examples2/stargan -b 1
+
+echo ''
+echo stylegan
+~/anaconda3/bin/python demo_dir.py -d examples2/stylegan/* -b 1
+
+echo ''
+echo stylegan2
+~/anaconda3/bin/python demo_dir.py -d examples2/stylegan2/* -b 1
+
+echo ''
+echo whichfaceisreal
+~/anaconda3/bin/python demo_dir.py -d examples2/whichfaceisreal -b 1
