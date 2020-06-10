a87a69f74e71c62d6bbc21e1338dc60071429afc
Richard Zhang <rizhang@rizhangs-MacBook-Pro.local>
2020-06-09 18:04:47 -0700

    crop script

---
+++ demo_dirs_crop224.sh
@@ -0,0 +1,52 @@
+
+echo ''
+echo biggan
+~/anaconda3/bin/python demo_dir.py -d examples2/biggan --crop 224
+
+echo ''
+echo crn
+~/anaconda3/bin/python demo_dir.py -d examples2/crn --crop 224
+
+echo ''
+echo cyclegan
+~/anaconda3/bin/python demo_dir.py -d examples2/cyclegan/* --crop 224
+
+echo ''
+echo deepfake
+~/anaconda3/bin/python demo_dir.py -d examples2/deepfake -b 1 --crop 224
+
+echo ''
+echo gaugan
+~/anaconda3/bin/python demo_dir.py -d examples2/gaugan --crop 224
+
+echo ''
+echo imle
+~/anaconda3/bin/python demo_dir.py -d examples2/imle --crop 224
+
+echo ''
+echo progan
+~/anaconda3/bin/python demo_dir.py -d examples2/progan/* --crop 224
+
+echo ''
+echo san
+~/anaconda3/bin/python demo_dir.py -d examples2/san -b 1 --crop 224
+
+echo ''
+echo seeingdark
+~/anaconda3/bin/python demo_dir.py -d examples2/seeingdark -b 1 --crop 224
+
+echo ''
+echo stargan
+~/anaconda3/bin/python demo_dir.py -d examples2/stargan --crop 224
+
+echo ''
+echo stylegan
+~/anaconda3/bin/python demo_dir.py -d examples2/stylegan/* --crop 224
+
+echo ''
+echo stylegan2
+~/anaconda3/bin/python demo_dir.py -d examples2/stylegan2/* --crop 224
+
+echo ''
+echo whichfaceisreal
+~/anaconda3/bin/python demo_dir.py -d examples2/whichfaceisreal --crop 224
